a1e0dac57500b61b74a2068895c058615d1ce3c2
iceniveth <ken11zer01@gmail.com>
2023-08-28 23:59:04 +0800

    Persist todos state with Postgresql

--- capstone/backend/routes/todo.js
+++ capstone/backend/routes/todo.js
@@ -1,26 +1,23 @@
 import express from "express";
 import { z } from "zod";
+import camelcaseKeys from "camelcase-keys";
+import { sql } from "../db.js";
 
 const todosRouter = express.Router();
 
-let todos = [
-  { id: 1, task: "wash dishes", isCompleted: false },
-  { id: 2, task: "watch tv", isCompleted: true },
-];
-
 const TodoSchema = z.object({
-  id: z.number(),
   task: z.string(),
   isCompleted: z.boolean(),
 });
 
-todosRouter.get("/", (req, res) => {
-  res.status(200).send(todos);
+todosRouter.get("/", async (req, res) => {
+  const todos = await sql`SELECT * FROM todos;`;
+  res.status(200).send(todos.map((todo) => camelcaseKeys(todo)));
 });
 
-todosRouter.get("/:id", (req, res) => {
+todosRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const foundTodo = todos.find((todo) => todo.id === Number(id));
+  const [foundTodo] = await sql`SELECT * FROM todos WHERE id = ${Number(id)};`;
 
   if (foundTodo) {
     res.status(200).send(foundTodo);
@@ -29,12 +26,8 @@ todosRouter.get("/:id", (req, res) => {
   }
 });
 
-todosRouter.post("/", (req, res) => {
-  const newTodo = {
-    ...req.body,
-    id: new Date().getTime(),
-  };
-
+todosRouter.post("/", async (req, res) => {
+  const newTodo = req.body;
   const parsedResult = TodoSchema.safeParse(newTodo);
 
   if (!parsedResult.success) {
@@ -46,34 +39,41 @@ todosRouter.post("/", (req, res) => {
     );
   }
 
-  todos.push(newTodo);
-  res.status(201).send(newTodo);
+  const [createdTodo] =
+    await sql`INSERT INTO todos (task, is_completed) VALUES (${newTodo.task}, ${newTodo.isCompleted}) RETURNING *`;
+
+  // todos.push(newTodo);
+  res.status(201).send(camelcaseKeys(createdTodo));
 });
 
-todosRouter.put("/:id", (req, res) => {
+todosRouter.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { task, isCompleted } = req.body;
-  const foundIndex = todos.findIndex((todo) => todo.id === Number(id));
 
-  if (foundIndex > -1) {
-    const updatedTodo = {
-      ...todos[foundIndex],
-      task,
-      isCompleted,
-    };
-    todos[foundIndex] = updatedTodo;
+  const [updatedTodo] = await sql`
+    UPDATE  todos 
+    SET     task = ${task}
+            , is_completed = ${isCompleted}
+            , updated_at = NOW()
+    WHERE   id = ${Number(id)}
+    RETURNING *
+  `;
+
+  if (updatedTodo) {
     res.status(200).send(updatedTodo);
   } else {
     res.status(404).send("todo not found");
   }
 });
 
-todosRouter.delete("/:id", (req, res) => {
+todosRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const foundIndex = todos.findIndex((todo) => todo.id === Number(id));
 
-  if (foundIndex > -1) {
-    todos = todos.filter((_, index) => index !== foundIndex);
+  const [deletedTodo] = await sql`DELETE FROM todos WHERE id = ${Number(
+    id
+  )} RETURNING *`;
+
+  if (deletedTodo) {
     res.status(204).send("");
   } else {
     res.status(404).send("todo not found");
